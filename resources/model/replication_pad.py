be0393eaaac1f94716d0cec37464a9aeff9e0883
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2024-03-23 16:53:37 +0100

    Add ReplicationPad1d layer to handle 1D tensor replication padding

---
+++ resources/model/replication_pad.py
@@ -0,0 +1,13 @@
+from torch import nn, Tensor
+import torch
+
+
+class ReplicationPad1d(nn.Module):
+    def __init__(self, padding) -> None:
+        super(ReplicationPad1d, self).__init__()
+        self.padding = padding
+
+    def forward(self, x: Tensor) -> Tensor:
+        replicate_padding = input[:, :, -1].unsqueeze(-1).repeat(1, 1, self.padding[-1])
+        output = torch.cat([x, replicate_padding], dim=-1)
+        return output
