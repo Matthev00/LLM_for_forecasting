b2d04003495292bfcdda92114271bdfb7ede2ba3
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2024-03-23 17:33:26 +0100

    Add PatchEmbedder class to embed_layers.py

--- resources/model/embed_layers.py
+++ resources/model/embed_layers.py
@@ -32,3 +32,22 @@ class TokenEmbedder(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         return self.tokenconv(x.permute(0, 2, 1)).transpose(1, 2)
+
+
+class PatchEmbedder(nn.Module):
+    def __init__(self, d_model, patch_len, stride, dropout):
+        super(PatchEmbedder, self).__init__()
+        self.patch_len = patch_len
+        self.stride = stride
+        self.padding_patch_layer = ReplicationPad1d((0, stride))
+        self.value_embedding = TokenEmbedder(patch_len, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        n_vars = x.shape[1]
+        x = self.padding_patch_layer(x)
+        x = x.unfold(dimension=-1, size=self.patch_len, step=self.stride)
+        x = torch.reshape(x, (x.shape[0] * x.shape[1], x.shape[2] * x.shape[3]))
+        x = self.value_embedding(x)
+        x = self.dropout(x)
+        return x, n_vars
