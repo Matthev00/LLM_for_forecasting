0ed6725ad0240403e4971aa5a98151988933ef8f
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2024-03-29 18:02:33 +0100

    Refactor TimeLLM class to include new layers and configurations

--- model/predictor.py
+++ model/predictor.py
@@ -13,26 +13,52 @@ class TimeLLM(nn.Module):
     def __init__(self, configs=None):
         super(TimeLLM, self).__init__()
 
-        # init
+        self.configs = configs
 
-        config = GPT2Config.from_pretrained("openai-community/gpt2")
-        # config.num_hidden_layers = configs.n_layers
-        config.output_attentions = True
-        config.output_hidden_states = True
-
-        self._set_llm_model(config)
+        self._set_llm_model()
         self._set_tokenizer()
         self._set_pad_token()
         self._freeze_llm()
         self.description = configs.content
 
-    def forward(self, x):
+        # layers
+        self.dropout = nn.Dropout(configs.dropout)
+
+        self.patch_embedder = PatchEmbedder(
+            configs.d_model, configs.patch_len, configs.stride, configs.dropout
+        )
+
+        self.word_embeddings = self.llm_model.get_input_embeddings().weight
+        vocab_size = self.word_embeddings.shape[0]
+        num_tokens = 1000
+        self.mapping_layer = nn.Linear(vocab_size, num_tokens)
+
+        self.reprogramming_layer = ReprogrammingLayer(
+            configs.d_model, configs.n_heads, configs.d_ff, configs.d_llm
+        )
+
+        self.patch_nums = int(
+            (configs.seq_len - configs.patch_len) / configs.stride + 2
+        )
+        self.head_nf = configs.d_ff * self.patch_nums
+
+        self.output_projection = FlattenHead(
+            configs.enc_in, self.head_nf, configs.pred_len, head_dropout=configs.dropout
+        )
+
+        self.normalize_layer = NormalizeLayer(configs.enc_in, affine=False)
+
+    def forward(self, x) -> Tensor:
         return self.forecast(x)
 
-    def forecast(self, x):
+    def forecast(self, x) -> Tensor:
         return x
 
-    def _set_llm_model(self, config):
+    def _set_llm_model(self):
+        config = GPT2Config.from_pretrained("openai-community/gpt2")
+        config.num_hidden_layers = self.configs.n_layers
+        config.output_attentions = True
+        config.output_hidden_states = True
         try:
             self.llm_model = GPT2Model.from_pretrained(
                 "openai-community/gpt2",
