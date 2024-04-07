f67a0cba6826bd4a639c1d8253cd245c6a0f6d60
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2024-04-07 14:53:14 +0200

    Add set_seeds function in utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+import random
+
+
+def set_seeds(seed: int = 42):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
