904f35e658fc5399795ac2b725da4c3af290e711
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2024-05-24 17:14:31 +0100

    added valid_step

--- engine.py
+++ engine.py
@@ -1,6 +1,8 @@
 import torch
+from torch.utils.tensorboard import SummaryWriter  # noqa 5501
 
 from tqdm import tqdm
+from typing import Tuple, Dict, List
 
 
 def train(
@@ -14,12 +16,24 @@ def train(
     mae_metric: torch.nn.Module,
     epochs: int = 10,
     device: torch.device = "cuda:0",
-):
+) -> Dict[str, List[float]]:
+    results = {
+        "train_loss": [],
+        "valid_loss": [],
+    }
     for epoch in range(epochs):
-        train_loss = train_step(model, train_loader, optim, scheduler, criterion, mae_metric)
+        train_loss = train_step(
+            model, train_loader, optim, scheduler, criterion, mae_metric
+        )
         # Update scheduler
         valid_loss = valid_step(model, valid_loader, criterion, mae_metric)
+
+        results["train_loss"].append(train_loss)
+        results["valid_loss"].append(valid_loss)
         # print info
+
+        # add tensorboard writer
+    return results
 
 
 def train_step(
@@ -69,5 +83,35 @@ def valid_step(
     criterion: torch.nn.Module,
     mae_metric: torch.nn.Module,
     device: torch.device = "cuda:0",
-):
-    pass
+) -> Tuple[float, float]:
+    model.eval()
+    valid_loss, mae_loss = 0.0, 0.0
+
+    with torch.no_grad():
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in tqdm(
+            enumerate(valid_loader)
+        ):
+            batch_x = batch_x.float().to(device)
+            batch_y = batch_y.float().to(device)
+            batch_x_mark = batch_x_mark.float().to(device)
+            batch_y_mark = batch_y_mark.float().to(device)
+
+            dec_inp = (
+                torch.zeros_like(batch_y[:, -args.pred_len :, :]).float().to(device)
+            )
+            dec_inp = (
+                torch.cat([batch_y[:, : args.label_len, :], dec_inp], dim=1)
+                .float()
+                .to(device)
+            )
+
+            outputs = model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+
+            f_dim = -1 if args.features == "MS" else 0
+            outputs = outputs[:, -args.pred_len :, f_dim:]
+            batch_y = batch_y[:, -args.pred_len :, f_dim:]
+            loss = criterion(outputs, batch_y)
+            valid_loss += loss.item()
+            mae_loss += mae_metric(outputs, batch_y).item()
+
+    return valid_loss / len(valid_loader), mae_loss / len(valid_loader)
